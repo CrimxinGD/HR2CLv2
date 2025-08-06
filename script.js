544d98f7e0cc2f93c7fec12dc13a6043d654f439
CrimxinGD <michaelsheppard584@gmail.com>
2025-08-05 21:36:56 -0230

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch('leaderboard.json')
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById('challenge-list');
+    data.forEach(entry => {
+      const div = document.createElement('div');
+      div.className = 'challenge-entry';
+      div.innerHTML = `
+        <h2>${entry.name}</h2>
+        <p><strong>Creator:</strong> ${entry.creator}</p>
+        <p><strong>Verifier:</strong> ${entry.verifier}</p>
+        <p><a href="${entry.video}" target="_blank">Watch Video</a></p>
+      `;
+      list.appendChild(div);
+    });
+  });
